4a1549195e6e0a0ccb8b0f962a46baa7f60c4b7f
Jayanth MKV <mkvjayanth1@gmail.com>
2024-04-14 23:39:15 +0530

    webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint:{
-       ignoreDuringBuilds:true
-    },
-    node: {
-        fs:"false"
-     },
-     typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
+   eslint: {
+      ignoreDuringBuilds: true
+   },
+   node: {
+      fs: "false"
+   },
+   typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+   },
+   webpack5: true,
+   webpack: (config) => {
+      config.resolve.fallback = {
+         fs: false,
+         net: false,
+         dns: false,
+         child_process: false,
+         tls: false,
+      };
+
+      return config;
+   },
 };
 
 export default nextConfig;
