1ce3dff31f64932cdc777fe0ed9155d81ebc0600
Jayanth MKV <mkvjayanth1@gmail.com>
2024-04-27 12:01:30 +0530

    app

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
-export const BACKEND_URL="http://localhost:8000"
-export const FRONTEND_URL="http://localhost:3001"
-export const PUBLISH_URL="http://localhost:3001"
-// export const BACKEND_URL="https://mascca-backend.onrender.com"
-// export const FRONTEND_URL="https://mascaa-user.vercel.app"
-// export const PUBLISH_URL="https://mascaa-user.vercel.app"
+// export const BACKEND_URL="http://localhost:8000"
+// export const FRONTEND_URL="http://localhost:3001"
+// export const PUBLISH_URL="http://localhost:3001"
+export const BACKEND_URL="https://mascca-backend.onrender.com"
+export const FRONTEND_URL="https://mascaa-user.vercel.app"
+export const PUBLISH_URL="https://mascaa-user.vercel.app"
 export const BASE_URL=BACKEND_URL
 
 export const parseJwt = (token:string)=>{
